4fe84592171caeae6be780c5ed0e182ba7369629
MonikaSimeonova <118007413+MonikaSimeonova@users.noreply.github.com>
2022-12-01 22:41:27 +0200

    Create loadingBar.js

---
+++ JS Fundamentals/Functions/Exercise/loadingBar.js
@@ -0,0 +1,16 @@
+function loadingBar(number) {
+
+    let percentCount = '%'.repeat(number / 10);
+    let dotCount = '.'.repeat(10 - (number / 10));
+    // let dotCount = '.'.repeat(10 - percentCount.length);
+
+    if (number === 100) {
+        console.log('100% Complete!');
+    } else if (number < 100) {
+        console.log(`${number}% [${percentCount}${dotCount}]`)
+        console.log('Still loading...');
+
+    }
+
+}
+loadingBar(30)
