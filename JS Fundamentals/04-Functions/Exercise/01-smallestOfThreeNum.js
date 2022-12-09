45331484c71c050cb69153dc01f115952be7568b
MonikaSimeonova <118007413+MonikaSimeonova@users.noreply.github.com>
2022-12-09 21:28:51 +0200

    renamed

---
+++ JS Fundamentals/04-Functions/Exercise/01-smallestOfThreeNum.js
@@ -0,0 +1,7 @@
+function smallestFromThreeNum(firstNum,secondNum,lastNum){
+
+    let smallestNum = Math.min(firstNum,secondNum,lastNum);
+    console.log(smallestNum);
+
+}
+smallestFromThreeNum(2,3,5)
