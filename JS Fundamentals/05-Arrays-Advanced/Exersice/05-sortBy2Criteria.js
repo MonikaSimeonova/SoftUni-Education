9064b7e9f3ed3ad9dcf5e1383443aebd257964ef
MonikaSimeonova <118007413+MonikaSimeonova@users.noreply.github.com>
2022-12-11 16:50:07 +0200

    added conditions

--- JS Fundamentals/05-Arrays-Advanced/Exersice/05-sortBy2Criteria.js
+++ JS Fundamentals/05-Arrays-Advanced/Exersice/05-sortBy2Criteria.js
@@ -1,8 +1,17 @@
-function sortBy2Criteria(arr){
+function sortBy2Criteria(arr) {
 
-    let sortedArray = arr.sort((a, b) =>{
+    let sortedArray = arr.sort((a, b) => {
         return a.length - b.length || a.localeCompare(b)
     });
     console.log(sortedArray.join('\n'));
 }
 sortBy2Criteria(['Isacc', 'Theodor', 'Jack', 'Harrison', 'George'])
+
+/*Write a function that orders an array of strings, 
+by their length in ascending order as primary criteria,
+and by alphabetical value in ascending order as second criteria. 
+The comparison should be case-insensitive.
+
+The input comes as an array of strings.
+
+The output is the ordered array of strings, each on a separate line */
